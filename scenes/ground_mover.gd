da94460b0fd60f2d986399b1c25584a3cd0643a4
Ahmed Alnour <ahmed2699@gmail.com>
2024-07-08 09:27:54 +0200

    made movement framerate independent

--- scenes/ground_mover.gd
+++ scenes/ground_mover.gd
@@ -3,15 +3,15 @@ extends Node2D
 @onready var tile1 = $TileMap
 @onready var tile2 = $TileMap2
 
-var speed = 8
+var speed = 480.192
 func _ready() -> void:
 	get_tree().get_first_node_in_group("player").died.connect(func(): speed = 0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	tile1.translate(Vector2.LEFT * speed)
-	tile2.translate(Vector2.LEFT * speed)
+	tile1.translate(Vector2.LEFT * speed * delta)
+	tile2.translate(Vector2.LEFT * speed * delta)
 	
 	for tile in [tile1, tile2]:
 		if tile.position.x < -540:
